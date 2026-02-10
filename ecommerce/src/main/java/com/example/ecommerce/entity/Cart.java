75ee086ec6b38477c68e74740620ba897c91fe37
iRahmanG <rahmanmr039@gmail.com>
2026-02-10 21:32:51 +0530

    Add Cart and CartItem with mapping

---
+++ ecommerce/src/main/java/com/example/ecommerce/entity/Cart.java
@@ -0,0 +1,51 @@
+package com.example.ecommerce.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime createdAt;
+
+    @OneToMany(
+            mappedBy = "cart",
+            cascade=CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<CartItem> items=new ArrayList<>();
+
+    protected Cart(){
+
+    }
+    public Cart(){
+        this.createdAt=LocalDateTime.now();
+    }
+    public Long getId(){
+        return id;
+    }
+    public LocalDateTime getCreatedAt(){
+        return createdAt;
+    }
+
+    public List<CartItem> getItems(){
+        return items;
+    }
+
+    public void addItem(CartItem item){
+        item.setCart(this);
+        items.add(item);
+    }
+
+    public void remove(CartItem item){
+        items.remove(item);
+    }
+
+}
---
+++ ecommerce/src/main/java/com/example/ecommerce/entity/CartItem.java
@@ -0,0 +1,63 @@
+package com.example.ecommerce.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+
+import java.math.BigDecimal;
+
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Min(1)
+    private int quantity;
+
+    @Column(nullable = false)
+    private BigDecimal priceAtAdd;
+
+    @ManyToOne(fetch= FetchType.LAZY)
+    @JoinColumn(name="cart_id",nullable=false)
+    private Cart cart;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="product_id",nullable = false)
+    private Product product;
+
+    protected CartItem(){
+        //JPA
+    }
+    public  CartItem(Product product,int quantity){
+        this.product=product;
+        this.quantity=quantity;
+        this.priceAtAdd=product.getPrice();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public BigDecimal getPriceAtAdd() {
+        return priceAtAdd;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void increaseQuantity(int amount){
+        if(amount<=0){
+            throw new IllegalArgumentException("Quantity must be positive");
+        }
+        this.quantity+=amount;
+    }
+
+    public void setCart(Cart cart){
+        this.cart=cart;
+    }
+}
