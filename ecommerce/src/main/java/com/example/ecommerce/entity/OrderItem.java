b729bbfcd0429da20c27113cbdd24842cd1ae01c
iRahmanG <rahmanmr039@gmail.com>
2026-02-12 00:20:12 +0530

    Added OrderItem

--- ecommerce/src/main/java/com/example/ecommerce/entity/OrderItem.java
+++ ecommerce/src/main/java/com/example/ecommerce/entity/OrderItem.java
@@ -1,4 +1,60 @@
 package com.example.ecommerce.entity;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="order_items")
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="order_id",nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id",nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    public OrderItem(){ }
+
+    public OrderItem(Product product, int quantity, BigDecimal price) {
+        this.product = product;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
 }
