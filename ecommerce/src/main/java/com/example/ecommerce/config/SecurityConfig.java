fb44ad32e78838828f156f6f70a0f83fd7256811
iRahmanG <rahmanmr039@gmail.com>
2026-02-14 15:59:06 +0530

    feat(auth) implemented JwtAuthenticationFilter

--- ecommerce/src/main/java/com/example/ecommerce/config/SecurityConfig.java
+++ ecommerce/src/main/java/com/example/ecommerce/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.example.ecommerce.config;
 
+import com.example.ecommerce.security.CustomUserDetailsService;
+import com.example.ecommerce.security.JwtAuthenticationFilter;
+import com.example.ecommerce.service.JwtUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,13 +18,15 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtFilter) throws Exception{
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(jwtFilter,
+                        org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
     @Bean
@@ -35,6 +40,13 @@ public class SecurityConfig {
         return configuration.getAuthenticationManager();
     }
 
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(
+            JwtUtil jwtUtil,
+            CustomUserDetailsService userDetailsService) {
+
+        return new JwtAuthenticationFilter(jwtUtil, userDetailsService);
+    }
 
 
 
--- ecommerce/src/main/java/com/example/ecommerce/service/JwtUtil.java
+++ ecommerce/src/main/java/com/example/ecommerce/service/JwtUtil.java
@@ -1,11 +1,13 @@
 package com.example.ecommerce.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
 import java.util.Date;
 
 @Service
@@ -22,5 +24,28 @@ public class JwtUtil {
                 .setExpiration((new Date(System.currentTimeMillis() + EXPIRATION)))
                 .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()), SignatureAlgorithm.HS256)
                 .compact();
+    }
+    public String extractUsername(String token){
+        return extractAllClaims(token).getSubject();
+    }
+    public boolean validateToken(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token){
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
     }
 }
